44b59b749c28f0071205cb0036a98ba0b51f2d3b
PauloLuisF <pauloluis.f@gmail.com>
2017-09-17 16:10:26 -0300

    Adicionando novas alterações

--- app/Client.php
+++ app/Client.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Client extends Model
 {
-    //
+    //Declarando quals inputs do form
+    protected $fillable = {
+    	'name',
+    	'email'
+    };
 }
